f96851258d62b7bba05ee2513c5354c5b8cf5a83
scmbr <romvasily@mail.ru>
2026-03-03 23:31:19 +0900

    feat: add postgres client (connection)

---
+++ internal/infrastructure/postgres/client.go
@@ -0,0 +1,33 @@
+package postgres
+
+import (
+	"fmt"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/jmoiron/sqlx"
+)
+
+type Config struct {
+	Host     string
+	Port     int
+	Username string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+func NewPostgresDB(cfg Config) (*sqlx.DB, error) {
+	dsn := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=%s",
+		cfg.Host, cfg.Port, cfg.Username, cfg.DBName, cfg.Password, cfg.SSLMode)
+
+	db, err := sqlx.Connect("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(25)
+	db.SetConnMaxLifetime(5 * 60 * 1e9)
+
+	return db, nil
+}
