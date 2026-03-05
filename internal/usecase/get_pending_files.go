a3370995934bab7cbd40b081d09231aa14b4eca6
scmbr <romvasily@mail.ru>
2026-03-05 20:23:14 +0900

    feat: add get pending files usecase

---
+++ internal/usecase/get_pending_files.go
@@ -0,0 +1,21 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/scmbr/device-tsv-processor/internal/domain"
+	"github.com/scmbr/device-tsv-processor/internal/repository"
+)
+
+type GetPendingFiles struct {
+	fileRepo  repository.FileRecordRepository
+	batchSize int
+}
+
+func NewGetPendingFiles(fileRepo repository.FileRecordRepository, batchSize int) *GetPendingFiles {
+	return &GetPendingFiles{fileRepo: fileRepo, batchSize: batchSize}
+}
+
+func (uc *GetPendingFiles) Execute(ctx context.Context) ([]*domain.FileRecord, error) {
+	return uc.fileRepo.GetPending(ctx, uc.batchSize)
+}
