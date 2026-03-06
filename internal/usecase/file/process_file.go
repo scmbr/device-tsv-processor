f6941fa63708b0ee5835296165370074ecaecafe
scmbr <romvasily@mail.ru>
2026-03-07 00:11:07 +0900

    feat: add document creating in ProcessFile UseCase

--- internal/usecase/file/process_file.go
+++ internal/usecase/file/process_file.go
@@ -6,29 +6,36 @@ import (
 	"github.com/scmbr/device-tsv-processor/internal/domain"
 	"github.com/scmbr/device-tsv-processor/internal/errs"
 	"github.com/scmbr/device-tsv-processor/internal/repository"
+	"github.com/scmbr/device-tsv-processor/pkg/logger"
 )
 
 type ProcessFile struct {
-	fileRepo    repository.FileRecordRepository
-	messageRepo repository.DeviceMessageRepository
-	errorRepo   repository.ParseErrorRepository
-	txManager   repository.TxManager
-	parser      TSVParser
+	fileRepo     repository.FileRecordRepository
+	deviceRepo   repository.DeviceRepository
+	messageRepo  repository.DeviceMessageRepository
+	documentRepo repository.DocumentRepository
+	errorRepo    repository.ParseErrorRepository
+	txManager    repository.TxManager
+	parser       TSVParser
 }
 
 func NewProcessFile(
 	fileRepo repository.FileRecordRepository,
 	messageRepo repository.DeviceMessageRepository,
+	deviceRepo repository.DeviceRepository,
 	errorRepo repository.ParseErrorRepository,
+	documentRepo repository.DocumentRepository,
 	txManager repository.TxManager,
 	parser TSVParser,
 ) *ProcessFile {
 	return &ProcessFile{
-		fileRepo:    fileRepo,
-		messageRepo: messageRepo,
-		errorRepo:   errorRepo,
-		txManager:   txManager,
-		parser:      parser,
+		fileRepo:     fileRepo,
+		messageRepo:  messageRepo,
+		errorRepo:    errorRepo,
+		documentRepo: documentRepo,
+		deviceRepo:   deviceRepo,
+		txManager:    txManager,
+		parser:       parser,
 	}
 }
 
@@ -45,11 +52,27 @@ func (uc *ProcessFile) Execute(ctx context.Context, input ProcessFileInput) erro
 	}
 
 	records, parseErrors, err := uc.parser.Parse(ctx, input.Path)
+
 	if err != nil {
 		_ = uc.fileRepo.MarkFailed(ctx, input.FileID, err.Error())
 		return errs.Wrap(op, err)
 	}
-
+	if len(records) == 0 && len(parseErrors) == 0 {
+		return uc.fileRepo.MarkFailed(ctx, input.FileID, "file is empty or invalid")
+	}
+	for _, m := range records {
+		device, err := uc.deviceRepo.CreateIfNotExists(ctx, &domain.Device{
+			GUID:   m.UnitGUID,
+			InvID:  m.InvID,
+			MQTT:   m.MQTT,
+			Status: "",
+		})
+		if err != nil {
+			_ = uc.fileRepo.MarkFailed(ctx, input.FileID, err.Error())
+			return errs.Wrap(op, err)
+		}
+		m.DeviceID = device.ID
+	}
 	err = uc.txManager.WithTx(ctx, func(txCtx context.Context) error {
 
 		if len(records) > 0 {
@@ -70,7 +93,30 @@ func (uc *ProcessFile) Execute(ctx context.Context, input ProcessFileInput) erro
 		_ = uc.fileRepo.MarkFailed(ctx, input.FileID, err.Error())
 		return errs.Wrap(op, err)
 	}
+	for _, m := range records {
+		doc, err := domain.NewDocument(
+			m.UnitGUID,
+			"pdf",
+			domain.DocumentStatusPending,
+			nil,
+		)
+		if err != nil {
 
+			logger.Error("failed to create document object", err, map[string]interface{}{
+				"unitGUID": m.UnitGUID,
+			})
+			continue
+		}
+
+		_, err = uc.documentRepo.CreateIfNotExists(ctx, doc)
+		if err != nil {
+
+			logger.Error("failed to create document in DB", err, map[string]interface{}{
+				"unitGUID": m.UnitGUID,
+			})
+			continue
+		}
+	}
 	if err := uc.fileRepo.UpdateStatus(ctx, input.FileID, domain.FileRecordStatusProcessed); err != nil {
 		return errs.Wrap(op, err)
 	}
