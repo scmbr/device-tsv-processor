f12b309c9a4b6e88f8c761a61c13bdd2155120a7
scmbr <romvasily@mail.ru>
2026-03-08 00:30:49 +0900

    feat: add simple health check endpoint and handler

--- internal/delivery/http/handler.go
+++ internal/delivery/http/handler.go
@@ -1,9 +1,10 @@
 package http
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	v1_handlers "github.com/scmbr/device-tsv-processor/internal/delivery/http/handlers/v1"
-	"github.com/scmbr/device-tsv-processor/internal/delivery/http/middleware"
 	"github.com/scmbr/device-tsv-processor/internal/usecase"
 )
 
@@ -17,12 +18,17 @@ func NewHandler(ucs *usecase.UseCases) *Handler {
 
 func (h *Handler) Init() *gin.Engine {
 	router := gin.Default()
-	router.Use(gin.Recovery(), gin.Logger(), middleware.Error())
+	router.Use(gin.Recovery(), gin.Logger())
 	h.initAPI(router)
 	return router
 }
 func (h *Handler) initAPI(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
+	v1.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusAccepted, gin.H{
+			"msg": "pong",
+		})
+	})
 	deviceMessagesHandler := v1_handlers.NewDeviceMessageHandler(h.ucs.DeviceMessage.GetDeviceMessages)
 	messages := v1.Group("/messages")
 	{
